068c3e2baf3e2bc6801846a1c7b88d22f1ec0687
ctrlcat0x <developer.sahilrana@gmail.com>
2025-11-01 12:54:15 +0530

    reset repo

--- README.md
+++
@@ -1,5 +0,0 @@
-## Start History
-
-[![Star History Chart](https://api.star-history.com/svg?repos=iam-sahil/odyssey&type=Date)](https://star-history.com/#bytebase/star-history&Date)
-
-Commit Check-Shr3kx
--- registry/new-york-v4/examples/test-component-demo.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-export default function TestComponent() {
-  return (
-    <div className="bg-background w-full overflow-hidden rounded-lg border">
-      <h1>Hello World</h1>
-    </div>
-  )
-}
